b5b12de106ef2f3a128af33227ee6e88c3aa3b37
newmanje11 <jevonte_n@hotmail.com>
2018-11-24 11:30:03 -0500

    questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,155 @@
-// var smashQuestion = [
-//   "What is the newest Super Smash Bros. to be released this year?",
-//   "How many character are gonna be in the newwest edition?",
-//   "What is the name of one of Super Smash Bros. most famous pro players?",
-//   "Who is the name and face of the creation of the Super Smash Bros. series?",
-//   "Which of the new characters announced was once a boss character for the Smash Bros. Wii UI?"
+var smashQuestion = [
+  {
+    question: "What is the newest Super Smash Bros. game going to be released this year?",
+    answers: {
 
+      a: "Super Smash Bros. Battle Royale",
+      b: "Super smash Bros. Wii U",
+      c: "Super smash Bros. Ultimate",
+      d: "super smash Bros. Melee"
 
-// ];
+    }, correctAnswer: "c"
+  },
+  {
+    question: "How many character are gonna be in the newwest edition?",
+    answers: {
+
+      a: 10,
+      b: 20,
+      c: 55,
+      d: 74
+
+    }, correctAnswer: "d"
+  },
+  {
+    question: "What is the name of one of Super Smash Bros. most famous pro players?",
+    answers: {
+
+      a: "Zero",
+      b: "Mewtwo King",
+      c: "Mango",
+      d: "Hungry Box"
+
+    }, correctAnswer: "a"
+  },
+  {
+    question: "Who is the name and face of the creation of the Super Smash Bros. series?",
+    answers: {
+
+      a: "Reggie Fils-Aimé",
+      b: "Masahiro Sakurai",
+      c: "Satoru Iwata",
+      d: "Shigeru Miyamoto"
+
+    }, correctAnswer: "b"
+  },
+  {
+  question: "Which of the new characters announced was once a boss character for the Smash Bros. Wii U",
+    answers: {
+
+      a: "King DeDede",
+      b: "Boswer",
+      c: "King K. Rool",
+      d: "Ridley"
+
+    }
+  }
+  
+
+]; 
 // var smashChoices = [
 
 // ];
 
-// // wait for the page to load
-// $(document).ready(function () {
+// wait for the page to load
+$(document).ready(function () {
 
-//   $(".startBtn").on("click", function () {
+  $(".startBtn").on("click", function () {
 
-//     $(".start").hide();
+    console.log('youve pressed it')
 
-//     $(".submit").show();
+    $(".content").hide();
 
-//     $(".question").show();
+    $(".submit").show();
+
+    $(".questions").show();
+
+  });
+  })
 
 
-//     timestart();
+    timestart();
 
     
 
-//     // getting timer to start
+    // getting timer to start
 
-//     function timestart() {
-//       clearInterval(intervalId);
+    function timestart() {
+      clearInterval(intervalId);
 
-//       var time = 60;
-//       var intervalId;
-//       intervalId = setInterval(timeDown, 1000);
-//       function timeDown() {
-//         time--;
-//         $(".time").html("<h2>Time remaining: " + time + "</h2>");
+      var time = 60;
+      var intervalId;
+      intervalId = setInterval(timeDown, 1000);
+      function timeDown() {
+        time--;
+        $(".time").html("<h2>Time remaining: " + time + "</h2>");
 
-//         if (time === 0) {
-//           clearInterval(intervalId);
+        if (time === 0) {
+          alert('Times up');
+          clearInterval(intervalId);
 
-//         }
+        }
 
-//       }
+      }
+    }
+    
+    startGame(smashQuestion);
 
-//       function startGame() {
-//         var arrayLength = smashQuestion.length;
-//         for (var i = 0; i < arrayLength; i++) {
-//           $(".question").append("<p><h2>"+smashQuestion[i]+"</p></h2>");
-//         }
-//       }
-//       startGame();
+      function startGame(smashQuestion) {
+
+        var output = []; 
+        var answers;
+
+
+
+       
+        for (var i = 0; i < smashQuestion.length; i++) {
+
+            answers = [];
+
+            for ( letter in smashQuestion[i].answers) {
+
+              answers.push(
+                '<label>'
+                  + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                  + letter + ': '
+                  + smashQuestion[i].answers[letter]
+                + '</label>'
+              );
+            }
+
+            output.push(
+              '<div class="question">' + smashQuestion[i].question + '</div>'
+              + '<div class="answers">' + answers.join(' ') + '</div>'
+            );
+          }
+                 $('.questions').append(output.join(' '));
+        }
+   
+
+      
+           
+      //     var question= $("<div>");
+      //     question.addClass("questionDiv")
+
+      //     question.html("<p><h3>"+smashQuestion[i]+"</p></h3>");
+
+      //     $(".questions").append(question);
+
+      //     console.log(question);
+      //   }
+      // }
+      
 
 
 
